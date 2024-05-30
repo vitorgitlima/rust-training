5fa81f7b0b09e743e19d039f42e98f51e49f7d12
VitorLima <vitorliima18@gmail.com>
2024-05-30 16:29:33 -0300

    first commit

---
+++ target/debug/deps/variables-743852129020c6dd.d
@@ -0,0 +1,5 @@
+/home/vitorlima/developer/personal/rust-training/variables/target/debug/deps/variables-743852129020c6dd: src/main.rs
+
+/home/vitorlima/developer/personal/rust-training/variables/target/debug/deps/variables-743852129020c6dd.d: src/main.rs
+
+src/main.rs:
---
+++ target/debug/variables.d
@@ -0,0 +1,1 @@
+/home/vitorlima/developer/personal/rust-training/variables/target/debug/variables: /home/vitorlima/developer/personal/rust-training/variables/src/main.rs
